0f93c81dfa4eaf40d0d064cd7cea1b63b3c77d3a
Lucas Leite <leitelucas470@gmail.com>
2024-10-31 14:30:55 -0300

    JQuey added

--- ComentCRUD.js
+++ ComentCRUD.js
@@ -25,14 +25,13 @@ class Coment {
   }
 
   setContent() {
-    const inContent = document.querySelector("#comentInput");
-    return inContent.value;
+    return $("#coment-form-text").val();
   }
 
   setDate() {
     const date = new Date();
     const datePtBr =
-      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+      date.getDate() + "/" + (date.getMonth()) + "/" + date.getFullYear();
     return datePtBr;
   }
 }
@@ -40,9 +39,10 @@ class Coment {
 // CREATE
 function createComent() {
   const coment = new Coment();
+  createComentHTML(coment.idNumber, coment);
   saveComent(coment);
-  createComentHTML(coment);
 }
+$("#coment-form-button").click(createComent);
 
 function saveComent(coment) {
   let comentStorage = JSON.parse(localStorage.getItem("coments"));
@@ -52,66 +52,100 @@ function saveComent(coment) {
   localStorage.setItem("coments", JSON.stringify(comentStorage));
 }
 
-function createComentHTML(coment) {
-  let comentStorage = JSON.parse(localStorage.getItem("coments"));
-  let idNumber = 1;
-  if (comentStorage)
-    idNumber = Number(Object.keys(comentStorage)[Object.keys(comentStorage).length - 1]);
+// function createComentHTML(coment) {
+//   let comentStorage = JSON.parse(localStorage.getItem("coments"));
+//   let idNumber = 1;
+//   if (comentStorage)
+//     idNumber = Number(Object.keys(comentStorage)[Object.keys(comentStorage).length - 1]);
 
-  let main = document.querySelector("main");
+//   let main = document.querySelector("main");
 
-  let commentMain = document.createElement("div");
-  commentMain.classList.add("comment");
-  commentMain.id = "comment_" + idNumber;
-  main.appendChild(commentMain);
+//   let commentMain = document.createElement("div");
+//   commentMain.classList.add("comment");
+//   commentMain.id = "comment_" + idNumber;
+//   main.appendChild(commentMain);
 
-  let commentHeader = document.createElement("div");
-  commentHeader.classList.add("comment-header");
-  commentMain.appendChild(commentHeader);
+//   let commentHeader = document.createElement("div");
+//   commentHeader.classList.add("comment-header");
+//   commentMain.appendChild(commentHeader);
 
-  let headerUser = document.createElement("div");
-  headerUser.classList.add("comment-header-user");
-  commentHeader.appendChild(headerUser);
+//   let headerUser = document.createElement("div");
+//   headerUser.classList.add("comment-header-user");
+//   commentHeader.appendChild(headerUser);
 
-  let userImg = document.createElement("img");
-  headerUser.appendChild(userImg);
+//   let userImg = document.createElement("img");
+//   headerUser.appendChild(userImg);
 
-  let userName = document.createElement("p");
-  userName.textContent = coment.sender;
-  headerUser.appendChild(userName);
+//   let userName = document.createElement("p");
+//   userName.textContent = coment.sender;
+//   headerUser.appendChild(userName);
 
-  let commentInfo = document.createElement("div");
-  commentInfo.classList.add("comment-header-info");
-  commentHeader.appendChild(commentInfo);
+//   let commentInfo = document.createElement("div");
+//   commentInfo.classList.add("comment-header-info");
+//   commentHeader.appendChild(commentInfo);
 
-  let infoDate = document.createElement("p");
-  infoDate.textContent = coment.date;
-  commentInfo.appendChild(infoDate);
+//   let infoDate = document.createElement("p");
+//   infoDate.textContent = coment.date;
+//   commentInfo.appendChild(infoDate);
 
-  let commentContent = document.createElement("input");
-  commentContent.classList.add("comment-content");
-  commentContent.setAttribute("type", "text");
-  commentContent.setAttribute("value", coment.content);
-  commentContent.disabled = true;
-  commentMain.appendChild(commentContent);
+//   let commentContent = document.createElement("input");
+//   commentContent.classList.add("comment-content");
+//   commentContent.setAttribute("type", "text");
+//   commentContent.setAttribute("value", coment.content);
+//   commentContent.disabled = true;
+//   commentMain.appendChild(commentContent);
 
-  let commentOptions = document.createElement("div");
-  commentOptions.classList.add("comment-options");
-  commentMain.appendChild(commentOptions);
+//   let commentOptions = document.createElement("div");
+//   commentOptions.classList.add("comment-options");
+//   commentMain.appendChild(commentOptions);
 
-  let optionEdit = document.createElement("input")
-  optionEdit.setAttribute("type", "button");
-  optionEdit.setAttribute("value", "Editar");
-  commentOptions.appendChild(optionEdit);
+//   let optionEdit = document.createElement("input")
+//   optionEdit.setAttribute("type", "button");
+//   optionEdit.setAttribute("value", "Editar");
+//   commentOptions.appendChild(optionEdit);
 
-  let optionDelete = document.createElement("input");
-  optionDelete.setAttribute("type", "button");
-  optionDelete.setAttribute("value", "Excluir");
-  optionDelete.addEventListener("click", function() {deleteComent(idNumber)});
-  commentOptions.appendChild(optionDelete);
+//   let optionDelete = document.createElement("input");
+//   optionDelete.setAttribute("type", "button");
+//   optionDelete.setAttribute("value", "Excluir");
+//   optionDelete.addEventListener("click", function() {deleteComent(idNumber)});
+//   commentOptions.appendChild(optionDelete);
+// }
+
+function createComentHTML(idNumber, coment) {  
+  let commentHTML = `
+    <div class='comment' id="comment_`+idNumber+`">
+      <div class='comment-header'>
+        <div class='comment-header-user'>
+          <img src=''>
+          <p>`+coment.sender+`</p>
+        </div>
+        <div class='comment-header-info'>
+          <p></p>
+          <p>`+coment.date+`</p>
+        </div>
+      </div>
+      <input type="text" disabled value="`+coment.content+`" class='comment-content'></input>`;
+
+  if(coment.sender == sessionStorage.getItem("activeUser")) {
+    commentHTML +=`
+      <div class='comment-options'>
+        <button id="comment-activate-edit_`+idNumber+`" onclick="activeEditComment(`+idNumber+`)">Editar</button>
+        <button id="comment-delete_`+idNumber+`" onclick="deleteComent(`+idNumber+`)">Excluir</button>
+        <button id="comment-confirm-edit_`+idNumber+`" onclick="updateComentContent(`+idNumber+`)" style="display: none;">Confirmar</button>
+      </div>
+    </div>`
+  } else {
+    commentHTML +=`
+    </div>`;
+  }    
+  $("main").append(commentHTML);
 }
 
 // UPDATE
+function activeEditComment(idNumber) {
+  
+}
+
 function updateComentContent(idNumber) {}
 
 // DELETE
@@ -123,14 +157,23 @@ function deleteComent(idNumber) {
 function deleteComentFromStorage(idNumber) {
   let comentStorage = JSON.parse(localStorage.getItem("coments"));
   delete comentStorage[idNumber];
-  if (Number(Object.keys(comentStorage).length) == 0) {
-    localStorage.removeItem("coments");
-    return;
-  }
+  if (Number(Object.keys(comentStorage).length) == 0)
+    return localStorage.removeItem("coments");
   localStorage.setItem("coments", JSON.stringify(comentStorage));
 }
 
 function deleteComentHTML(idNumber) {
-  let coment = document.querySelector("#comment_"+idNumber);
-  coment.remove();
+  $("#comment_"+idNumber).remove();
 }
+
+// INITIALIZE
+function loadAllComents() {
+  let comentStorage = JSON.parse(localStorage.getItem("coments"));
+  if (comentStorage)  
+    for (const [key, value] of Object.entries(comentStorage))
+      createComentHTML(key, value);
+    // for(idNumber=1; idNumber<=Object.keys(comentStorage).length; idNumber++)
+    //   if(comentStorage[idNumber])
+    //     createComentHTML(comentStorage[idNumber], idNumber);
+}
+loadAllComents();
